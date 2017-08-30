2baaa7060d9822b260aebb18857c8060b0cc3754
hua <yuchonghua@163.com>
2017-08-30 09:35:42 +0800

    add etMessage setErrorId

--- src/DdvException.php
+++ src/DdvException.php
@@ -16,12 +16,39 @@
     public function __construct( $message = '' , $errorId = 'UNKNOWN_ERROR' , $code = '500', $responseData = array() )
     {
       parent::__construct($message,$code);
-      empty($errorId)||$this->errorId = $errorId;
-      empty($responseData)||$this->responseData = $responseData;
+      $this->setErrorId($errorId)->setResponseData($responseData);
     }
     public function getErrorId(){
       $errorId = empty($this->errorId)?'UNKNOWN_ERROR':$this->errorId;
       return $errorId;
+    }
+    public function setErrorId($errorId = null){
+      if (empty($errorId)) {
+        $this->errorId = empty($this->errorId)?'UNKNOWN_ERROR':$this->errorId;
+      }else{
+        $this->errorId = $errorId;
+      }
+      return $this;
+    }
+    public function setMessage($message = null){
+      if (empty($message)) {
+        $this->message = empty($this->message)?'Unknown Error':$this->message;
+      }else{
+        $this->message = $message;
+      }
+      return $this;
+    }
+    public function setCode($code = null){
+      if (empty($code)) {
+        $this->code = empty($this->code)?0:$this->code;
+      }else{
+        $this->code = $code;
+      }
+      return $this;
+    }
+    public function setResponseData($responseData = null){
+      empty($responseData)||$this->responseData = $responseData;
+      return $this;
     }
     public function getResponseData(){
       return empty($this->responseData)?array():$this->responseData;
